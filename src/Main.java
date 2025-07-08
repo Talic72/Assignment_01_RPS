4575c7fd51d3a24e153f3352fb9dddca2e4fa8b1
Talic72 <EMORRISWORK72@GMAIL.COM>
2025-07-07 20:42:37 -0400

    fini

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,64 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public Main() {
     }
-}
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String player1 = "";
+        String player2 = "";
+
+        // first input
+        do {
+            System.out.println("Player 1, Please enter R, P, S for rock paper or scissors respectively: ");
+            player1 = in.nextLine();
+            //Validation check
+            if (!player1.equalsIgnoreCase("R") && !player1.equalsIgnoreCase("P") && !player1.equalsIgnoreCase("S")) {
+                System.out.println("That input is not valid, please enter a correct value: ");
+            }
+        } while (!player1.equalsIgnoreCase("R") && !player1.equalsIgnoreCase("P") && !player1.equalsIgnoreCase("S"));
+
+        //second input
+
+        do {
+            System.out.println("Player 2, Please enter R, P, S for rock paper or scissors respectively: ");
+            player2 = in.nextLine();
+            //Validation check
+            if (!player2.equalsIgnoreCase("R") && !player2.equalsIgnoreCase("P") && !player2.equalsIgnoreCase("S")) {
+                System.out.println("That input is not valid, please enter a correct value: ");
+            }
+            } while (!player2.equalsIgnoreCase("R") && !player2.equalsIgnoreCase("P") && !player2.equalsIgnoreCase("S"));
+
+
+        //tie check
+        if (player1.equalsIgnoreCase(player2)) {
+            System.out.println(player1 + " Vs. " + player2 + ": Its a tie!");
+        }
+        //player 1 scissors check
+        else if (player1.equalsIgnoreCase("S")) {
+            if (player2.equalsIgnoreCase("R")) {
+                System.out.println(player1 + " Vs. " + player2 + ": Rock crushes Scissors, player 2 wins!");
+            } else {
+                System.out.println(player1 + " Vs. " + player2 + ": Scissors cut Paper, player 1 wins!");
+            }
+        }
+        //player 1 Rock check
+        else if (player1.equalsIgnoreCase("R")) {
+            if (player2.equalsIgnoreCase("S")) {
+                System.out.println(player1 + " Vs. " + player2 + ": Rock crushes Scissors, player 1 wins!");
+            } else {
+                System.out.println(player1 + " Vs. " + player2 + ": Paper covers Rock, player 2 wins!");
+            }
+        //player 1 paper check
+        } else if (player1.equalsIgnoreCase("P")) {
+            if (player2.equalsIgnoreCase("S")) {
+                System.out.println(player1 + " Vs. " + player2 + ": Scissors cuts Paper, player 2 wins!");
+            } else {
+                System.out.println(player1 + " Vs. " + player2 + ": Paper covers Rock, player 1 wins!");
+                    }
+                }
+            }
+    }
+
